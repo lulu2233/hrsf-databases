8c880018ad24477e42f8d711a2f22cb0dd339e3d
Hack Reactor Student <student@hackreactor.com>
2020-02-26 18:24:43 -0800

    create database and tables

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,28 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+// var connection = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'student',
+//   password : 'student',
+//   database : 'chat'
+// });
 
+// connection.connect();
+
+// connection.query('INSERT INTO messages(id,  text, room) values(0, "Hello", "Lobby");', function (error, results, fields) {
+//   if (error) throw error;
+//   // console.log('The solution is: ', results[0].solution);
+// });
+
+// // pool.query(`CREATE TABLE messages (
+// //   id INT AUTO_INCREMENT,
+// //   text VARCHAR(255),
+// //   room VARCHAR(100),
+// //   PRIMARY KEY(id))`, function (error, results, fields) {
+// //   if (error) throw error;
+
+// //   console.log('succesfully called db server');
+// // });
+
+// console.log('called');
--- server/schema.sql
+++ server/schema.sql
@@ -1,12 +1,37 @@
+DROP DATABASE IF EXISTS chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+CREATE TABLE users (
   /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  name VARCHAR(255),
+  PRIMARY KEY(id)
 );
 
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  content VARCHAR(255),
+  room VARCHAR(100),
+  fromUser INT,
+  FOREIGN KEY (fromUser) REFERENCES users(id),
+  PRIMARY KEY(id)
+);
 /* Create other tables and define schemas for them here! */
+
+CREATE TABLE friends (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  user1 INT,
+  user2 INT,
+  FOREIGN KEY(user1) REFERENCES users(id),
+  FOREIGN KEY(user2) REFERENCES users(id),
+  PRIMARY KEY(id)
+);
+
 
 
 
